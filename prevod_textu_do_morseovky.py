ef37ec57242f759715f25ca6fec5b2c896eb0205
Marek Macháček <machacek1marek@gmail.com>
2025-12-08 16:11:30 +0100

    bla

--- prevod_textu_do_morseovky.py
+++ prevod_textu_do_morseovky.py
@@ -3,9 +3,6 @@ import tkinter as tk
 from slovnik_prevodnik import slovník_morse
 import unicodedata
 
-#zadaní textu
-text1 = vstup_pole.get() 
-
 #redukce textu diakritika a velká
 def odstran_diakritiku(text):
     normalizovany_text = unicodedata.normalize('NFD', text)
@@ -28,32 +25,7 @@ def prevod_do_morse():
         else:
             morse_vysledek += "[" + znak + "]" + "/"       
     print("vysledný text",morse_vysledek)
-    # 4. Vymaž předchozí výsledek a zobraz nový
-        vystup_pole.delete("1.0", tk.END)  # vymaže obsah
-        vystup_pole.insert("1.0", morse_vysledek)  # vloží nový text
-
-# Vytvoření hlavního okna
-okno = tk.Tk()
-okno.title("Převodník do morseovky")
-okno.geometry("500x300")  # šířka x výška
-
-# Popisek pro vstup
-vstup_label = tk.Label(okno, text="Zadej text k převodu:", font=("Arial", 12))
-vstup_label.pack(pady=5)
-
-# Hlavní funkce pro převod
-def prevod_do_morse1():
-    morse_vysledek = ""
-    for znak in text2:
-        if znak in slovník_morse:
-            morse_vysledek += slovník_morse[znak]
-        else:
-            morse_vysledek += "[" + znak + "]" + "/"       
-    print("vysledný text",morse_vysledek)
     
-# 4. Vymaž předchozí výsledek a zobraz nový
-vystup_pole.delete("1.0", tk.END)  # vymaže obsah
-vystup_pole.insert("1.0", morse_vysledek)  # vloží nový text
 
 # Vytvoření hlavního okna
 okno = tk.Tk()
@@ -64,19 +36,20 @@ okno.geometry("500x300")  # šířka x výška
 vstup_label = tk.Label(okno, text="Zadej text k převodu:", font=("Arial", 12))
 vstup_label.pack(pady=5)
 
->>>>>>> origin/main
 # Vstupní textové pole
 vstup_pole = tk.Entry(okno, width=50, font=("Arial", 11))
 vstup_pole.pack(pady=5)
 
 # Tlačítko pro převod
-<<<<<<< HEAD
-tlacitko = tk.Button(okno, text="Převést do morseovky", command=prevod_do_morse()), 
 tlacitko = tk.Button(okno, text="Převést do morseovky", command=prevod_do_morse, 
                      bg="lightblue", font=("Arial", 11))
 tlacitko.pack(pady=10)
 
 # Popisek pro výstup
+    # 4. Vymaž předchozí výsledek a zobraz nový
+    vystup_pole.delete("1.0", tk.END)  # vymaže obsah
+    vystup_pole.insert("1.0", morse_vysledek)  # vloží nový text
+
 vystup_label = tk.Label(okno, text="Morseovka:", font=("Arial", 12))
 vystup_label.pack(pady=5)
 
