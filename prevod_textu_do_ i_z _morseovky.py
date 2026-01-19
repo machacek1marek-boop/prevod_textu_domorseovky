0425f445ecdd22344019564374efbae9dfdc0a40
Marek Macháček <machacek1marek@gmail.com>
2026-01-19 16:33:18 +0100

    pridani a skoro funkční prevod tam i spet

--- prevod_textu_do_ i_z _morseovky.py
+++ prevod_textu_do_ i_z _morseovky.py
@@ -1,13 +1,22 @@
 # Import knihoven
 import tkinter as tk
 from slovnik_prevodnik import slovník_morse
+import unicodedata
+
+#redukce textu diakritika a velká
+def odstran_diakritiku(text):
+    normalizovany_text = unicodedata.normalize('NFD', text)
+    text_bez_diakritiky = "".join(
+        znak for znak in normalizovany_text
+        if unicodedata.category(znak) != 'Mn'
+    )
+
+    return text_bez_diakritiky.replace("đ", "d").replace("Đ", "D")
 
 # Vytvoření otočeného slovníku (Morse -> Písmeno)
 # Musíme odstranit lomítka z hodnot v původním slovníku pro správné porovnání
 slovnik_zpet = {hodnota.replace("/", ""): klic for klic, hodnota in slovník_morse.items()}# Ručně přidáme mezeru, která je v původním slovníku definovaná jako "/"
 slovnik_zpet[""] = " "
-
-
 
 # Hlavní funkce pro převod
 def prevod_z_morse():
@@ -29,31 +38,57 @@ def prevod_z_morse():
     vystup_pole.delete("1.0", tk.END)  # vymaže obsah
     vystup_pole.insert("1.0", morse_vysledek)  # vloží nový text
 
+
+# Hlavní funkce pro převod
+def prevod_do_morse():
+    text1 = vstup_pole.get()
+    print(text1)
+    text2 = odstran_diakritiku(text1).upper()
+    print("zredukovaný text:",text2)
+    morse_vysledek = ""
+    for znak in text2:
+        if znak in slovník_morse:
+            morse_vysledek += slovník_morse[znak]
+        else:
+            morse_vysledek += "[" + znak + "]" + "/"       
+    print("vysledný text",morse_vysledek)
+    #Vymaž předchozí výsledek a zobraz nový
+    vystup_pole.delete("1.0", tk.END)  # vymaže obsah
+    vystup_pole.insert("1.0", morse_vysledek)  # vloží nový text
+
 # Vytvoření hlavního okna
 okno = tk.Tk()
-okno.title("Převodník do morseovky")
+okno.title("Převodník do nebo z morseovky")
 okno.geometry("500x300")  # šířka x výška
 
 # Popisek pro vstup
-vstup_label = tk.Label(okno, text="Zadej text k převodu:", font=("Arial", 12))
+vstup_label = tk.Label(okno, text="Text:", font=("Arial", 12))
 vstup_label.pack(pady=5)
 
 # Vstupní textové pole
-vstup_pole = tk.Entry(okno, width=50, font=("Arial", 11))
+vstup_pole = tk.Text(okno, height=2, width=50, font=("Arial", 11))
 vstup_pole.pack(pady=5)
 
-# Tlačítko pro převod
-tlacitko = tk.Button(okno, text="Převést z morseovky", command=prevod_z_morse, 
+
+# Tlačítko pro převod do morse 
+tlacitko = tk.Button(okno, text="↓ Převést do morseovky ↓", command=prevod_do_morse, 
                      bg="lightblue", font=("Arial", 11))
 tlacitko.pack(pady=10)
 
+
+# Tlačítko pro převod nba text
+tlacitko = tk.Button(okno, text="↑ Převést na text ↑", command=prevod_do_morse, 
+                     bg="lightblue", font=("Arial", 11))
+tlacitko.pack(pady=10)
+
+
 # Popisek pro výstup
 vystup_label = tk.Label(okno, text="Morseovka:", font=("Arial", 12))
-vystup_label.pack(pady=5)
+vystup_label.pack(pady=15)
 
 # Výstupní textové pole (větší, pro více řádků)
 vystup_pole = tk.Text(okno, height=5, width=50, font=("Courier", 10))
-vystup_pole.pack(pady=5)
+vystup_pole.pack(pady=15)
 
 # Spuštění programu
 okno.mainloop()
